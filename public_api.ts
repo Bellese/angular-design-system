1550f5ad8e450f365597d4c38706b150e36419a6
Allison Roberts <aroberts@bellese.io>
2018-06-08 16:11:53 -0400

    export modal component

--- public_api.ts
+++ public_api.ts
@@ -6,6 +6,6 @@ export * from './src/app/modules/table/table.module'
 export * from './src/app/modules/modal/modal.module'
 export * from './src/app/directives/directive.module'
 export * from './src/app/directives/modal-host'
-export * from './src/app/modules/modal/modal/modal.component'
-export * from './src/app/directives/modal-host'
+export { AppModal } from './src/app/modules/modal/modal/modal.component'
+
 
