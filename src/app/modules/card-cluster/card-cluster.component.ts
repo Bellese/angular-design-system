c523bcb66d233f01b2e924077667aba0dbd3014e
jackmasterflex <jlopez1992@yahoo.com>
2019-01-23 15:37:06 -0500

    adding extra functionality and image to card cluster

--- src/app/modules/card-cluster/card-cluster.component.ts
+++ src/app/modules/card-cluster/card-cluster.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
     @Component({
         selector: 'app-card-cluster',
@@ -7,51 +7,86 @@ import { Component, OnInit, Input } from '@angular/core';
     })
 export class AppCardCluster implements OnInit {
 
-    @Input() cardArray = [
+    @Input() cardArray =
         {
-            'value': 1
-        },
-        {
-            'value': 2
-        },
-        {
-            'value': 3
-        },
-        {
-            'value': 4
-        },
-        {
-            'value': 5
-        },
-        {
-            'value': 6
-        }];
+            'mainTitle': 'STK-2',
+            'mainSub': 'Episodes',
+            'cluster': [
+                {
+                    'value': 13,
+                    'name': 'IPP Not Met'
+                },
+                {
+                    'value': 0,
+                    'name': 'IPP Met'
+                },
+                {
+                    'value': 1,
+                    'name': 'Denominator'
+                },
+                {
+                    'value': 16,
+                    'name': 'Denominator'
+                },
+                {
+                    'value': 85,
+                    'name': 'Numerator'
+                },
+                {
+                    'value': 3,
+                    'name': 'Den. Exceptions'
+                },
+                {
+                    'classValue': 'viewPerformanceIcon',
+                    'classButton': 'vpi',
+                    'name': 'View Performance',
+                    'className': 'vpi'
+                } 
+            ]   
+        };
     
+    @Output() passButton = new EventEmitter<any>();
+    
+    buttonClicked = "CardCluster0";
     numArray1 =[];
     numArray = [];
+    total = 0;
     
     constructor() { }
+    
+    passAction(e) {
+        this.passButton.emit(e);
+        this.buttonClicked = e.target.id;
+    }
 
     ngOnInit() {
-        if (this.cardArray.length % 4 === 0 || this.cardArray.length % 3 === 0) {
-            this.cardArray.map( x => {
-                if (this.cardArray.length % 3 === 0 && this.cardArray.length % 4 !== 0) {
+        //total
+        this.cardArray.cluster.map( x => {
+            (x.value && typeof x.value === 'number') ? this.total += x.value : null;
+        })
+        
+        //sequence 1 is for desktop layout
+        if (this.cardArray.cluster.length % 4 === 0 || this.cardArray.cluster.length % 3 === 0) {
+            this.cardArray.cluster.map( x => {
+                if (this.cardArray.cluster.length % 3 === 0 && this.cardArray.cluster.length % 4 !== 0) {
                     this.numArray1.push(Object.assign({sequence: 4}, x));
                 } else {
                     this.numArray1.push(Object.assign({sequence: 3}, x));
                 }
             })
         } else {
-            let rows = Math.floor(this.cardArray.length / 4);
-            this.cardArray.map( (x, ind) => {
+            let rows = Math.floor(this.cardArray.cluster.length / 4);
+            this.cardArray.cluster.map( (x, ind) => {
                 if((ind+1) <= rows * 4) {
                     this.numArray1.push(Object.assign({sequence: 3}, x));
                 } else {
-                    let seq = 12 / (this.cardArray.length % 4);
+                    let seq = 12 / (this.cardArray.cluster.length % 4);
                     this.numArray1.push(Object.assign({sequence: seq}, x));
                 }
             })
         }
+        
+        //sequence 2 is for mobile layout
         if (this.numArray1.length % 2 === 0) {
             this.numArray1.map( x => {
                 this.numArray.push(Object.assign({sequence2: 6}, x));
@@ -62,11 +97,13 @@ export class AppCardCluster implements OnInit {
                 if((ind+1) <= rows * 2) {
                     this.numArray.push(Object.assign({sequence2: 6}, x));
                 } else {
-                    let seq = 12 / (this.cardArray.length % 2);
+                    let seq = 12 / (this.cardArray.cluster.length % 2);
                     this.numArray.push(Object.assign({sequence2: seq}, x));
                 }
             })
         }
+        
+        console.log(this.numArray);
     }
 
 }
