7cd91c54a73f9500f6dad54fe79e8f91beb98d89
Pere Ozogu <kakarath@gmail.com>
2019-08-06 17:15:26 -0400

    Update tabs.component.ts

--- src/app/modules/tabs/tabs.component.ts
+++ src/app/modules/tabs/tabs.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent {
+export class TabsComponent implements OnInit {
+  
+  @Input parentAriaLabel: string;
+  
+  @Input childAriaLabel: string;
+  @Input routerPrefix: string = "/";
+  @Input childrouterLink: string;
+  @Input childAttrAriaPrefix: string = "rla";
+  @Input childAttrAriaSuffix: string = ".isActive";
+  @Input childAriaSelected: string;
+  
 }
