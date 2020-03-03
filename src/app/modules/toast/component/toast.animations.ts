a7f61b14ae913df23ee03c45cf4e219ed90dcd85
joshglazer-bellese <jglazer@bellese.io>
2020-03-03 14:32:49 -0500

    updated animation for toast component

--- src/app/modules/toast/component/toast.animations.ts
+++ src/app/modules/toast/component/toast.animations.ts
@@ -2,40 +2,9 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 
 export const Animations = {
     animations: [
-        trigger('flyInOut', [
+          trigger('flyInOut', [
             state('inactive', style({
-              opacity: 0,
-            })),
-            transition('inactive => active', animate('400ms ease-out', keyframes([
-              style({
-                transform: 'translate3d(100%, 0, 0) skewX(-30deg)',
-                opacity: 0,
-              }),
-              style({
-                transform: 'skewX(20deg)',
-                opacity: 1,
-              }),
-              style({
-                transform: 'skewX(-5deg)',
-                opacity: 1,
-              }),
-              style({
-                transform: 'none',
-                opacity: 1,
-              }),
-            ]))),
-            transition('active => removed', animate('400ms ease-out', keyframes([
-              style({
-                opacity: 1,
-              }),
-              style({
-                transform: 'translate3d(100%, 0, 0) skewX(30deg)',
-                opacity: 0,
-              }),
-            ]))),
-          ]),
-          trigger('flyInOutJazzy', [
-            state('inactive', style({
+              transform: 'translate3d(100%, 0, 0)',
               opacity: 0,
             })),
             transition('inactive => active', animate('400ms ease-out', keyframes([
