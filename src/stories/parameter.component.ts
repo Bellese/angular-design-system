6a5e9d50ec226e08147ea210798ee545fa0e67f5
joshglazer-bellese <jglazer@bellese.io>
2020-03-09 16:47:17 -0400

    Better documentation for the new table component Storybook component improvements and cleanup

--- src/stories/parameter.component.ts
+++ src/stories/parameter.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-storybook-parameter-component',
     template: `
         <strong>{{ parameter.name }}</strong>&nbsp;<em>({{ parameter.type }})</em>
         <div [innerHTML]="parameter.value"></div>
-        <div *ngIf="parameter.default" [innerHTML]="'Default: ' + parameter.default"></div>
+        <div *ngIf="optionsList.length">
+            <em>Options:</em>
+            <ul>
+                <li *ngFor="let option of optionsList">
+                    {{ option }}
+                    <span *ngIf="option === parameter.default"><em>(default)</em></span>
+                </li>
+            </ul>
+        </div>
+        <div *ngIf="parameter.default && !optionsList.length" [innerHTML]="'Default: ' + parameter.default"></div>
         <div *ngIf="parameter.properties">
             <em>Properties:</em>
             <ul>
@@ -18,6 +27,18 @@ import { Component, Input } from '@angular/core';
     styles: [],
 })
 
-export default class ParameterComponent {
+export default class ParameterComponent implements OnInit {
     @Input() parameter;
+
+    optionsList = [];
+
+    ngOnInit() {
+        if (this.parameter.options) {
+            for (const optionsListItem in this.parameter.options) {
+                if (this.parameter.options.hasOwnProperty(optionsListItem)) {
+                    this.optionsList.push(`${this.parameter.type}.${optionsListItem}`);
+                }
+            }
+        }
+    }
 }
