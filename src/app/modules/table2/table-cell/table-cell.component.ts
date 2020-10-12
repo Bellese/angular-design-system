c8b6cf4a8add672aff00b4b8d55330b43c902943
hg3337 <iadams@bellese.io>
2020-10-12 08:31:08 -0500

    buttonType

--- src/app/modules/table2/table-cell/table-cell.component.ts
+++ src/app/modules/table2/table-cell/table-cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 // Models
 import { TableCellModel, TableCellTypeEnum } from '../table.models';
@@ -6,38 +6,38 @@ import { TableCellModel, TableCellTypeEnum } from '../table.models';
 @Component({
   selector: 'app-table-cell-2',
   templateUrl: './table-cell.component.html',
-  styleUrls: ['./table-cell.component.css']
+  styleUrls: ['./table-cell.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-
 export class TableCellComponent {
-    @Input() tableCellModel: TableCellModel;
-    @Output() cellClick = new EventEmitter<any>();
+  @Input() tableCellModel: TableCellModel;
+  @Output() cellClick = new EventEmitter<any>();
 
-    TableCellTypeEnum = TableCellTypeEnum;
+  TableCellTypeEnum = TableCellTypeEnum;
 
-    cellClass() {
-        const classes = ['table_cell'];
-        if (this.tableCellModel.columnKey) {
-            classes.push(`table_cell_${this.tableCellModel.columnKey}`);
-        }
-        if (this.tableCellModel.class) {
-            classes.push(this.tableCellModel.class);
-        }
-        return classes;
+  cellClass() {
+    const classes = ['table_cell'];
+    if (this.tableCellModel.columnKey) {
+      classes.push(`table_cell_${this.tableCellModel.columnKey}`);
     }
-
-    checkboxClickEvent($event) {
-        this.cellClick.emit({
-            $event : $event,
-            tableCellModel: this.tableCellModel
-        });
+    if (this.tableCellModel.class) {
+      classes.push(this.tableCellModel.class);
     }
+    return classes;
+  }
 
-    buttonClickEvent($event) {
-        if (this.tableCellModel.buttonOnClick) {
-            this.tableCellModel.buttonOnClick($event);
-        } else {
-            console.warn('The button\'s click event was not configured.');
-        }
+  checkboxClickEvent($event) {
+    this.cellClick.emit({
+      $event: $event,
+      tableCellModel: this.tableCellModel,
+    });
+  }
+
+  buttonClickEvent($event) {
+    if (this.tableCellModel.buttonOnClick) {
+      this.tableCellModel.buttonOnClick($event);
+    } else {
+      console.warn("The button's click event was not configured.");
     }
+  }
 }
