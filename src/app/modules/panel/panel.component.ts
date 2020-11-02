85c4367c0405c982497b5f381d3c4324a14b6dce
hg3337 <iadams@bellese.io>
2020-11-02 13:02:42 -0600

    add plus action

--- src/app/modules/panel/panel.component.ts
+++ src/app/modules/panel/panel.component.ts
@@ -2,39 +2,45 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Animations } from '../animations/animations';
 
 @Component({
-    selector: 'app-panel',
-    templateUrl: './panel.component.html',
-    styleUrls: ['./panel.component.css'],
-    animations: [
-        Animations.animations
-    ]
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.css'],
+  animations: [Animations.animations],
 })
 export class AppPanelComponent implements OnInit {
+  @Output() panelClick = new EventEmitter<any>();
+  @Output() plusAction = new EventEmitter<any>();
+  @Input() title: string;
+  @Input() titleClass = 'ds-u-fill--primary-darkest ds-u-color--white';
+  @Input() extTitle = false;
+  @Input() extTitleClass: string;
+  @Input() expand = false;
+  @Input() expandedClass = 'ds-u-padding--2';
+  @Input() openAll = false;
+  @Input() dataAutoId: string;
+  @Input() plusClass = '';
+  @Input() bypassPanelState = false;
 
-    @Output() panelClick = new EventEmitter<any>();
-    @Input() title: string;
-    @Input() titleClass = 'ds-u-fill--primary-darkest ds-u-color--white';
-    @Input() extTitle = false;
-    @Input() extTitleClass: string;
-    @Input() expand = false;
-    @Input() expandedClass = 'ds-u-padding--2';
-    @Input() openAll = false;
-    @Input() dataAutoId: string;
-    @Input() plusClass = '';
+  clicked;
 
-    clicked;
+  constructor() {}
 
-    constructor() {
+  ngOnInit() {
+    if (this.expand) {
+      this.clicked = true;
     }
+  }
 
-    ngOnInit() {
-        if (this.expand) { this.clicked = true; }
+  panelStateChange(e) {
+    this.panelClick.emit(e);
+    if (!this.bypassPanelState) {
+      this.clicked = true;
+      this.expand = !this.expand;
     }
+  }
 
-    panelStateChange(e) {
-        this.panelClick.emit(e);
-        this.clicked = true;
-        this.expand = !this.expand;
-    }
-
+  plusActionClick(e) {
+    this.plusAction.emit(e);
+    this.clicked = true;
+  }
 }
