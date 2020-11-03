7691293224958fbc1d3966bc28fe631f09aa4f1d
hg3337 <iadams@bellese.io>
2020-11-03 13:42:10 -0600

    browser module should only be loaded once err

--- src/app/modules/calendar/calendar.module.ts
+++ src/app/modules/calendar/calendar.module.ts
@@ -1,30 +1,26 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { CalendarComponent } from "./calendar.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatInputModule } from "@angular/material/input";
-import { MatNativeDateModule } from "@angular/material/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { ChoiceModule } from "../choice/choice.module";
-import { TextFieldModule } from "../text-field/text-field.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './calendar.component';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChoiceModule } from '../choice/choice.module';
+import { TextFieldModule } from '../text-field/text-field.module';
 
 @NgModule({
   declarations: [CalendarComponent],
   imports: [
     CommonModule,
     FormsModule,
-    BrowserModule,
     MatDatepickerModule,
     MatInputModule,
     MatNativeDateModule,
-    BrowserAnimationsModule,
     FontAwesomeModule,
     ChoiceModule,
-    TextFieldModule
+    TextFieldModule,
   ],
-  exports: [CalendarComponent]
+  exports: [CalendarComponent],
 })
 export class CalendarModule {}
