807ca747bfe652dfa836076f7af4906c1eadbcc7
hg3337 <iadams@bellese.io>
2020-11-06 09:05:36 -0600

    fix calendar storybook

--- src/app/modules/calendar/calendar.component.ts
+++ src/app/modules/calendar/calendar.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
 
 // Models
-import { CalendarModel } from "./calendar.model";
-import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
+import { CalendarModel } from './calendar.model';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
-  selector: "app-calendar",
-  templateUrl: "./calendar.component.html",
-  styleUrls: ["./calendar.component.css"]
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent {
   @Input() calendarModel: CalendarModel;
@@ -22,47 +22,45 @@ export class CalendarComponent {
   constructor() {}
 
   validateDate(event) {
-    let date: moment.Moment = event && (moment as any).default(event.value);
-    let isEndDate: boolean = event && event.targetElement.name === "endDate";
+    const date: moment.Moment = event && (moment as any).default(event.value);
+    const isEndDate: boolean = event && event.targetElement.name === 'endDate';
 
     this.errorMessage = this.checkDate(date, isEndDate);
 
     if (!this.errorMessage && event) {
-      isEndDate
-        ? (this.calendarModel.endDate = event.value)
-        : (this.calendarModel.date = event.value);
+      isEndDate ? (this.calendarModel.endDate = event.value) : (this.calendarModel.date = event.value);
     }
     if (
       this.calendarModel.minDate &&
       (moment as any).default(this.calendarModel.date).isBefore(this.calendarModel.minDate)
     ) {
-      this.errorMessage = `Start date must be on or after ${(moment as any).default(
-        this.calendarModel.minDate
-      ).format("MM-DD-YYYY")}`;
+      this.errorMessage = `Start date must be on or after ${(moment as any)
+        .default(this.calendarModel.minDate)
+        .format('MM-DD-YYYY')}`;
     }
     if (
       this.calendarModel.maxDate &&
       (moment as any).default(this.calendarModel.date).isAfter(this.calendarModel.maxDate)
     ) {
-      this.errorMessage = `Start date must be on or before ${(moment as any).default(
-        this.calendarModel.maxDate
-      ).format("MM-DD-YYYY")}`;
+      this.errorMessage = `Start date must be on or before ${(moment as any)
+        .default(this.calendarModel.maxDate)
+        .format('MM-DD-YYYY')}`;
     }
     if (
       this.calendarModel.minEndDate &&
       (moment as any).default(this.calendarModel.endDate).isBefore(this.calendarModel.minEndDate)
     ) {
-      this.errorMessage = `End date must be on or after ${(moment as any).default(
-        this.calendarModel.minEndDate
-      ).format("MM-DD-YYYY")}`;
+      this.errorMessage = `End date must be on or after ${(moment as any)
+        .default(this.calendarModel.minEndDate)
+        .format('MM-DD-YYYY')}`;
     }
     if (
       this.calendarModel.maxEndDate &&
       (moment as any).default(this.calendarModel.endDate).isAfter(this.calendarModel.maxEndDate)
     ) {
-      this.errorMessage = `End date must be on before ${(moment as any).default(
-        this.calendarModel.maxEndDate
-      ).format("MM-DD-YYYY")}`;
+      this.errorMessage = `End date must be on before ${(moment as any)
+        .default(this.calendarModel.maxEndDate)
+        .format('MM-DD-YYYY')}`;
     }
     if (
       this.calendarModel.date &&
@@ -70,29 +68,30 @@ export class CalendarComponent {
       this.showEndDate &&
       (moment as any).default(this.calendarModel.date).isAfter(this.calendarModel.endDate)
     ) {
-      this.errorMessage = "Start date is after end date";
+      this.errorMessage = 'Start date is after end date';
     }
 
-    if (!this.errorMessage)
+    if (!this.errorMessage) {
       this.selectedDates.emit({
         dateId: `${this.calendarModel.id}Date`,
         endDateId: `${this.calendarModel.id}EndDate`,
         date: this.calendarModel.date,
-        endDate: this.calendarModel.endDate
+        endDate: this.calendarModel.endDate,
       });
+    }
   }
 
   checkDate(date: moment.Moment, endDate: boolean) {
     if (date && !date.isValid()) {
-      return !endDate
-        ? "Date is not a valid date"
-        : "End Date is not a valid date";
+      return !endDate ? 'Date is not a valid date' : 'End Date is not a valid date';
     }
     return null;
   }
 
   handleCheckbox(event) {
     this.showEndDate = !event.target.checked;
-    if (!this.showEndDate) this.calendarModel.endDate = null;
+    if (!this.showEndDate) {
+      this.calendarModel.endDate = null;
+    }
   }
 }
--- src/app/modules/calendar/calendar.stories.ts
+++ src/app/modules/calendar/calendar.stories.ts
@@ -1,32 +1,34 @@
 // Storybook
-import { storiesOf, moduleMetadata } from "@storybook/angular";
-import { StoriesModule } from "../../../stories/stories.module";
-import { defaultProps } from "../../../../.storybook/helpers";
+import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { StoriesModule } from '../../../stories/stories.module';
+import { defaultProps } from '../../../../.storybook/helpers';
 
 // Modules
-import { CalendarModule } from "./calendar.module";
+import { CalendarModule } from './calendar.module';
 
 // Models
-import { CalendarModel } from "./calendar.model";
+import { CalendarModel } from './calendar.model';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const calendarModel = new CalendarModel({
-  id: "calendar",
-  date: new Date("07/21/2020"),
-  endDate: new Date("08/21/2020"),
+  id: 'calendar',
+  date: new Date('07/21/2020'),
+  endDate: new Date('08/21/2020'),
   isCheckBox: true,
   isDateRange: true,
-  minDate: new Date("07/21/2020"),
-  maxDate: new Date("08/21/2020"),
-  minEndDate: new Date("08/21/2020"),
-  maxEndDate: new Date("09/21/2020")
+  minDate: new Date('07/21/2020'),
+  maxDate: new Date('08/21/2020'),
+  minEndDate: new Date('08/21/2020'),
+  maxEndDate: new Date('09/21/2020'),
 });
 const calendarNoBox = new CalendarModel({
-  id: "calendar",
-  isDateRange: true
+  id: 'calendar',
+  isDateRange: true,
 });
 const calendarModelNoRange = new CalendarModel({
-  id: "calendar",
-  dateLabel: "Pick your date"
+  id: 'calendar',
+  dateLabel: 'Pick your date',
 });
 
 const props = {
@@ -39,10 +41,10 @@ const props = {
 storiesOf('Components/Calendar', module)
   .addDecorator(
     moduleMetadata({
-      imports: [StoriesModule, CalendarModule]
+      imports: [StoriesModule, CalendarModule, BrowserModule, BrowserAnimationsModule],
     })
   )
-  .add("Intro", () => ({
+  .add('Intro', () => ({
     template: `
             <app-storybook-component-intro-component
                 [imports]="imports"
@@ -52,122 +54,116 @@ storiesOf('Components/Calendar', module)
     props: {
       imports: [
         {
-          modules: ["CalendarModule"],
-          file: "@bellese/angular-design-system",
-          ngmodule: "imports"
-        }
+          modules: ['CalendarModule'],
+          file: '@bellese/angular-design-system',
+          ngmodule: 'imports',
+        },
       ],
       parameters: [
         {
-          name: "calendarModel",
-          type: "CalendarModel",
+          name: 'calendarModel',
+          type: 'CalendarModel',
           value: "Use this to override the component's default settings.",
           properties: [
             {
-              name: "id",
-              type: "string",
-              value: "Use this for ID"
+              name: 'id',
+              type: 'string',
+              value: 'Use this for ID',
             },
             {
-              name: "dateLabel",
-              type: "string",
-              value: "Use this for label of first date"
+              name: 'dateLabel',
+              type: 'string',
+              value: 'Use this for label of first date',
             },
             {
-              name: "endDateLabel",
-              type: "string",
-              value: "Use this for label of second date"
+              name: 'endDateLabel',
+              type: 'string',
+              value: 'Use this for label of second date',
             },
             {
-              name: "isDateRange",
-              type: "boolean",
-              value: "Use this to decide if second date is needed"
+              name: 'isDateRange',
+              type: 'boolean',
+              value: 'Use this to decide if second date is needed',
             },
             {
-              name: "isCheckBox",
-              type: "string",
-              value: "Use this to decide if checkbox is needed"
+              name: 'isCheckBox',
+              type: 'string',
+              value: 'Use this to decide if checkbox is needed',
             },
             {
-              name: "date",
-              type: "Date",
-              value: "Use this for initial date"
+              name: 'date',
+              type: 'Date',
+              value: 'Use this for initial date',
             },
             {
-              name: "endDate",
-              type: "string",
-              value: "Use this for initial endDate"
+              name: 'endDate',
+              type: 'string',
+              value: 'Use this for initial endDate',
             },
             {
-              name: "dataAutoId",
-              type: "string",
-              value: "Use this for testing ID"
+              name: 'dataAutoId',
+              type: 'string',
+              value: 'Use this for testing ID',
             },
             {
-              name: "ariaLabelDate",
-              type: "string",
-              value:
-                "Use this to further specify button to the screen reader for date input"
+              name: 'ariaLabelDate',
+              type: 'string',
+              value: 'Use this to further specify button to the screen reader for date input',
             },
             {
-              name: "ariaLabelEndDate",
-              type: "string",
-              value:
-                "Use this to further specify button to the screen reader for end date input"
+              name: 'ariaLabelEndDate',
+              type: 'string',
+              value: 'Use this to further specify button to the screen reader for end date input',
             },
             {
-              name: "minDate",
-              type: "Date",
-              value:
-                "Use this to set a minimum acceptable date"
+              name: 'minDate',
+              type: 'Date',
+              value: 'Use this to set a minimum acceptable date',
             },
             {
-              name: "maxDate",
-              type: "Date",
-              value:
-                "Use this to set a maximum acceptable date"
+              name: 'maxDate',
+              type: 'Date',
+              value: 'Use this to set a maximum acceptable date',
             },
             {
-              name: "minEndDate",
-              type: "Date",
-              value:
-                "Use this to set a minimum acceptable date for the end date"
+              name: 'minEndDate',
+              type: 'Date',
+              value: 'Use this to set a minimum acceptable date for the end date',
             },
             {
-              name: "maxEndDate",
-              type: "Date",
-              value:
-                "Use this to set a maximum acceptable date for the end date"
+              name: 'maxEndDate',
+              type: 'Date',
+              value: 'Use this to set a maximum acceptable date for the end date',
             },
-          ]
-        }
-      ]
-    }
+          ],
+        },
+      ],
+    },
   }))
-  .add("Normal", () => ({
+  .add('Normal', () => ({
     template: `
-            <app-calendar 
+            <app-calendar
                 [calendarModel] = 'calendarModelNoRange'
                 (selectedDates) = 'handleEvent($event)'>
             </app-calendar>
         `,
-    props
+    props,
   }))
-  .add("Date Range", () => ({
+  .add('Date Range', () => ({
     template: `
-            <app-calendar 
+            <app-calendar
                 [calendarModel] = 'calendarNoBox'
                 (selectedDates) = 'handleEvent($event)'>
             </app-calendar>
         `,
-    props
+    props,
   }))
-  .add("DateRange with Checkbox", () => ({
+  .add('DateRange with Checkbox', () => ({
     template: `
-            <app-calendar 
+            <app-calendar
                 [calendarModel] = 'calendarModel'
                 (selectedDates) = 'handleEvent($event)'>
             </app-calendar>
         `,
-    props
+    props,
   }));
