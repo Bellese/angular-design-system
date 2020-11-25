3b8f7cd2a2b30e092c3d774ffe54228d7b007682
ijadams1 <iadams@bellese.io>
2020-11-25 11:34:17 -0800

    debounce time configurable on calendar

--- src/app/modules/calendar/calendar.model.ts
+++ src/app/modules/calendar/calendar.model.ts
@@ -1,24 +1,25 @@
 import { AngularDesignSystemModel } from '../../models/angular-design-system.models';
-import { Moment } from "moment";
+import { Moment } from 'moment';
 
-export class CalendarModel extends AngularDesignSystemModel{
-    id: string = '';
-    dateLabel: string = "Start Date";
-    endDateLabel: string = "End Date";
-    isDateRange: boolean = false;
-    isCheckBox: boolean = false;
-    dataAutoId: string = "calendarPicker";
-    date: Date;
-    endDate: Date;
-    ariaLabelDate: string = "Date input";
-    ariaLabelEndDate: string = 'End Date input';
-    minDate: Date | undefined = undefined;
-    maxDate: Date | undefined = undefined;
-    minEndDate: Date | undefined = undefined;
-    maxEndDate: Date | undefined = undefined;
+export class CalendarModel extends AngularDesignSystemModel {
+  id: string = '';
+  dateLabel: string = 'Start Date';
+  endDateLabel: string = 'End Date';
+  isDateRange: boolean = false;
+  isCheckBox: boolean = false;
+  dataAutoId: string = 'calendarPicker';
+  debounceTime?: number;
+  date: Date;
+  endDate: Date;
+  ariaLabelDate: string = 'Date input';
+  ariaLabelEndDate: string = 'End Date input';
+  minDate: Date | undefined = undefined;
+  maxDate: Date | undefined = undefined;
+  minEndDate: Date | undefined = undefined;
+  maxEndDate: Date | undefined = undefined;
 
-    constructor(options?) {
-        super();
-        this.setOptions(options);
-    }
+  constructor(options?) {
+    super();
+    this.setOptions(options);
+  }
 }
