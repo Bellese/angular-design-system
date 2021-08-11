2d4f950eb8b8dc6e773542454a7ab25dc199c856
Josh Glazer <jglazer@bellese.io>
2021-08-11 16:14:16 -0400

    fix(linting): linted file to trigger ci build

--- src/app/pipes/graph-tooltip.pipe.ts
+++ src/app/pipes/graph-tooltip.pipe.ts
@@ -1,17 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'graphToolTip'
+  name: 'graphToolTip',
 })
-
 export class GraphToolTipPipe implements PipeTransform {
-
-    transform(items: any): any {
-
-        if (items === '') {
-            return 'N/A';
-        } else {
-            return items;
-        }
+  transform(items: any): any {
+    if (items === '') {
+      return 'N/A';
+    } else {
+      return items;
     }
+  }
 }
