0b95aa42371ee5f1c9af1b2d4d02fc07bc4ecb5f
Jim Rice <hitjim@gmail.com>
2022-08-25 14:03:12 -0500

    initial take on data-test directive

--- src/app/directives/data-test.directive.ts
+++ src/app/directives/data-test.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appDataTest]'
+  selector: '[hqrTestId]'
 })
-export class DataTestDirective {
+export class DataTestDirective implements OnInit {
+  @Input() hqrTestId!: string;
 
-  constructor() { }
+  element: any; // must be 'any' to match el.nativeElement
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.element = el.nativeElement;
+  }
+
+  ngOnInit(): void {
+    this.inputToAttribute()
+  }
+
+  inputToAttribute() {
+    this.renderer.setAttribute(this.element, 'data-test', this.hqrTestId);
+    this.renderer.removeAttribute(this.element, 'hqrTestId');
+  }
 }
